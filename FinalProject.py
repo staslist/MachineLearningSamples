27ee3e8255cc4a7135f6d6364549fa71f686e3eb
Stas <slistopa@uci.edu>
2017-12-29 01:06:01 -0800

    Added latest version of CS 273A final project.

--- FinalProject.py
+++ FinalProject.py
@@ -22,7 +22,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras import optimizers
+from keras.wrappers.scikit_learn import KerasClassifier
 from tensorflow.python.client import device_lib
+from sklearn.model_selection import GridSearchCV
 
 print(device_lib.list_local_devices())
 
@@ -39,7 +41,7 @@ Xtr, Xva, Ytr, Yva = ml.splitData(X, Y)
 Xtr, Ytr = ml.shuffleData(Xtr, Ytr)
 
 # Taking a subsample of the data so that trains faster.
-Xt, Yt = Xtr[:], Ytr[:] 
+Xt, Yt = Xtr[:10000], Ytr[:10000] 
 
 XtS, params = ml.rescale(Xt)
 XvS, _ = ml.rescale(Xva, params)
@@ -49,7 +51,6 @@ XteS, _ = ml.rescale(Xte, params)
 # loss = 'binary_crossentropy', optimizer = 'adam', metrics = 'accuracy'
 # and activation = 'relu' via references found online & trial/error
 
-'''
 scores = []
 num_hidden_layers = [1, 5, 10, 30, 50, 100]
 
@@ -70,17 +71,16 @@ for i in num_hidden_layers:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=1000)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=1000)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
 # without regularization ~10 hidden layers seem to deliver best results (as opposed to 1, 5, 30, 50...) 
 # with dropout regularization ~10 hidden layers still delivers best results
 
-'''
+
 scores = []
 num_hidden_neurons = [7, 14, 28, 56, 112, 224, 448]
 num_hidden_layers = 10
@@ -102,13 +102,13 @@ for i in num_hidden_neurons:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=1000)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=1000)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 
 # without regularization num hidden neurons = 14 performed the best (~70%)
 # most publications/textbooks on the topic suggest using number of neurons in hidden layers 
@@ -118,7 +118,7 @@ print(scores)
 
 # with dropout regularization num hidden neurons between 28 & 112 (inclusive) performed the best (~70%)
 # note: doubling number of neurons per hidden layer roughly doubles the runtime
-'''
+
 scores = []
 activations = ['relu', 'selu', 'elu', 'tanh', 'sigmoid']
 num_hidden_layers = 10
@@ -141,15 +141,15 @@ for acti in activations:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=1000)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=1000)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 # elu(70.4%) > tanh(70.26%) > selu > relu > sigmoid(68.9%). However, the differences were small.
-'''
+
 scores = []
 losses = ['mean_squared_logarithmic_error', 'mean_absolute_error', 'binary_crossentropy']
 num_hidden_layers = 10
@@ -172,15 +172,15 @@ for lo in losses:
     # Compile model
     model.compile(loss=lo, optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=1000)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=1000)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 # as expected binary cross entropy delivers best results
-'''
+
 scores = []
 rms = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
@@ -206,16 +206,16 @@ for opti in optimizers:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=opti, metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=1000)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=1000)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 # very similar results with all of the optimizers. Adamax performed slightly better than all 
 # other optimizers
-'''
+
 scores = []
 batch_sizes = [200, 500, 1000, 2000]
 num_hidden_layers = 10
@@ -238,16 +238,16 @@ for b_s in batch_sizes:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='Adamax', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=100, batch_size=b_s)
+    model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=b_s)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 # very similar results, batch size of 500 seems to work best
 
-'''
+
 scores = []
 num_epochs = [100, 250, 500, 750]
 num_hidden_layers = 10
@@ -270,18 +270,18 @@ for n_e in num_epochs:
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='Adamax', metrics=['accuracy'])
     # Fit the model
-    model.fit(XtS, Yt, epochs=n_e, batch_size=500)
+    model.fit(XtS, Yt, epochs=n_e, verbose=0, batch_size=500)
     # evaluate the model
     score = model.evaluate(XvS, Yva)
     # append accuracy only
     scores.append(score[1]*100)
 print(scores)
-'''
+
 # No noticeable improvement between 100 & 500 epochs. 
 
-# The hypermarameters can be tuned further. However, due to time constraints I decided 
+# The hypermarameters can be tuned much further. However, due to time constraints I decided 
 # to stop here. 
-
+'''
 num_hidden_layers = 10;
 num_hidden_neurons = 14;
 
@@ -298,17 +298,103 @@ model.add(Dense(1, activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='Adamax', metrics=['accuracy'])
 # Fit the model
-model.fit(XtS, Yt, epochs=100, batch_size=500)
+model.fit(XtS, Yt, epochs=100, verbose=0, batch_size=500)
 # evaluate the model
 score = model.evaluate(XvS, Yva)
 print(score)
-
+'''
 # If time try selecting (there are noisy / not relevant features) or adding features
 # (there is interdependence between features)
+# Can safely assume from the problem write up that all of the features are relevant
+'''
+XtSP = ml.transforms.fpoly(XtS, degree=2, bias=False)
+XvSP = ml.transforms.fpoly(XvS, degree=2, bias=False)
+XteSP = ml.transforms.fpoly(XteS, degree=2, bias=False)
+# there are now 119 features: original 14 features + 14 square features + additional cross features such as x1*x2
+# Rescale the data matrix so that the features have similar ranges / variance
+XtSP, params = ml.transforms.rescale(XtSP)
+XvSP, _ = ml.transforms.rescale(XvSP, params)
+XteS, _ = ml.transforms.rescale(XvSP, params)
+'''
+'''
+num_hidden_layers = 10;
+num_hidden_neurons = 119;
 
+model = Sequential()
+    
+model.add(Dense(119, input_dim=119, activation='elu'))
+model.add(Dropout(0.2))
+model.add(Dense(119, activation='elu'))
+model.add(Dropout(0.2))
+j = 0
+while j < num_hidden_layers:
+    model.add(Dense(num_hidden_neurons, activation='elu'))
+    model.add(Dropout(0.2))
+    j = j + 1
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='Adamax', metrics=['accuracy'])
+# Fit the model
+model.fit(XtSP, Yt, epochs=100, batch_size=500)
+# evaluate the model
+score = model.evaluate(XvSP, Yva)
+print(score)
+'''
+
+'''
+# Function to create model, required for KerasClassifier
+def create_model(dropout_rate=0.0, neurons = 119):
+    # create model
+    num_hidden_layers = 10;
+    num_hidden_neurons = neurons;
+    
+    model = Sequential()
+        
+    model.add(Dense(119, input_dim=119, activation='elu'))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(119, activation='elu'))
+    model.add(Dropout(dropout_rate))
+    j = 0
+    while j < num_hidden_layers:
+        model.add(Dense(num_hidden_neurons, activation='elu'))
+        model.add(Dropout(dropout_rate))
+        j = j + 1
+    model.add(Dense(1, activation='sigmoid'))
+    
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='Adamax', metrics=['accuracy'])
+    return model
+
+
+model = KerasClassifier(build_fn=create_model, verbose=0)
+# define the grid search parameters
+batch_size = [500, 1000, 2000]
+epochs = [100, 400, 700]
+#optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+#init_mode = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
+#activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+dropout_rate = [0, 0.15, 0.35, 0.5]
+neurons = [40, 80, 119]
+param_grid = dict(batch_size=batch_size, epochs=epochs, dropout_rate=dropout_rate, neurons=neurons)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+print('Began automated hyperparameter grid search.')
+grid_result = grid.fit(XtSP, Yt)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+'''
+
+
+'''
 # calculate predictions
 predictions = model.predict(XteS)
 print(predictions)
 
 Yte = np.vstack((np.arange(Xte.shape[0]), predictions[:, 0])).T
 np.savetxt('data/Y_submit.txt', Yte, '%d, %.2f', header = 'ID,Prob1', comments='', delimiter=',')
+'''
